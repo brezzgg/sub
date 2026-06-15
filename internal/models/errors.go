bea34862d958b50788f3c5f2c10b703068c5ba80
brezzgg <afonin1003@gmail.com>
2026-06-15 16:37:58 +0000

    feat: remove old models

--- internal/models/errors.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "errors"
-
-var (
-	ErrNotFound = errors.New("subscription not found")
-	ErrExpired  = errors.New("subscription expired")
-	ErrInternal = errors.New("internal error")
-)
--- internal/models/id.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "regexp"
-
-var subRe = regexp.MustCompile(`^[a-zA-Z0-9_-]{128,128}$`)
-
-func ValidateSubId(id string) bool {
-	return subRe.MatchString(id)
-}
